939632f06aa17ced1205301dd284eeda21dc392e
TPFTMON <tpftmoon@gmail.com>
2026-04-01 10:36:52 +0200

    ex00: little adjustments

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -10,7 +10,7 @@ typename T::iterator easyfind(T& container, int to_find) {
     typename T::iterator it = std::find(container.begin(), container.end(), to_find);
     if (it == container.end())
         throw std::runtime_error("to_find not found in container");
-    return it;
+    return (it);
 }
 
 #endif
