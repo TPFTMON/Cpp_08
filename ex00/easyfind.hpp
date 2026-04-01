1c5e13fdc70d660cf7632b03de0ef1fc8f74f036
TPFTMON <tpftmoon@gmail.com>
2026-04-01 10:27:26 +0200

    ex00: easyfind implementation

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -5,35 +5,12 @@
 # include <iostream>
 
 
-// Messages:
-# define EASYFIND_MSG "\e[0;32measyfind\n\e[0m"
-
-# define DEF_CONSTR_MSG "\e[0;33mDefault Constructor\e[0m called of "
-# define COPY_CONSTR_MSG "\e[0;33mCopy Constructor\e[0m called of "
-# define COPY_ASSIGN_OP_MSG "\e[0;35mCopy assignment operator\e[0m called of "
-# define DESTR_MSG "\e[0;31mDestructor\e[0m called of "
-
-
-// Classes:
-class easyfind{
-
-    private:
-        // ... some private stuff
-
-    public:
-        // Orthodox Canonical Form:
-        easyfind();
-        easyfind(const easyfind &to_copy);
-        easyfind& operator=(const easyfind &assign);
-        ~easyfind();
-
-        // Other member functions:
-        // ... some members
-
-};
-
-
-// Other:
-// ...
+template <typename T>
+typename T::iterator easyfind(T& container, int to_find) {
+    typename T::iterator it = std::find(container.begin(), container.end(), to_find);
+    if (it == container.end())
+        throw std::runtime_error("to_find not found in container");
+    return it;
+}
 
 #endif
