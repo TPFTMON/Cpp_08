36c391986f569048ae2136c5a77bfe8272463494
TPFTMON <tpftmoon@gmail.com>
2026-04-29 13:20:22 +0200

    ex00: exception has been changed to be more precise

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -3,15 +3,27 @@
 
 // Includes:
 # include <algorithm>    // for std::find
-// # include <exception>
+# include <exception>
 
+
+// Classes:
+class EasyFindException : public std::exception{
+
+    private:
+        const char* _msg;
+
+    public:
+        EasyFindException(const char* msg) : _msg(msg) {}
+        virtual const char* what() const throw() { return (_msg); }
+
+};
 
 template <typename T>
 typename T::iterator easyfind(T& container, int to_find){
 
     typename T::iterator it = std::find(container.begin(), container.end(), to_find);
     if (it == container.end()){
-        throw std::runtime_error("to_find not found in container");
+        throw EasyFindException("find() did not detect the element in container");
     }
     return (it);
 }                        // I don't use iterators to search manually. I use find. It does all the work.
